2b940acc63e9974008fa4f5f0bd87c78daf5d24a
ClementBregeon <clment.bregeon@gmail.com>
2024-10-24 22:05:35 +0200

    Login, Historique avec Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'; 
 import 'package:awesome_notifications/awesome_notifications.dart';
 import "package:universal_html/html.dart" as universal_html;
 import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await initializeService();
-  AwesomeNotifications().initialize(
-    null, // Icône de la notif
+  await initializeDateFormatting('fr_FR', null);
+  Intl.defaultLocale = 'fr_FR';
+  await AwesomeNotifications().initialize(
+    'resource://drawable/res_app_icon', // Default icon for notifications
     [
       NotificationChannel(
-        channelKey: 'sound_channel',
+        channelKey: 'basic_channel',
         channelName: 'Pomodoro Timer',
         channelDescription: 'Notification channel for Pomodoro timer',
         defaultColor: Color(0xFF9D50E8),
         ledColor: Colors.white,
-        soundSource: 'resource://raw/merci_mehdi.mp3',
+        soundSource: 'resource://raw/merci_mehdi',
         enableVibration: true,
         criticalAlerts: true,
-        playSound: true
+        playSound: true,
+        importance: NotificationImportance.High,
+        icon: 'resource://drawable/clock',
       ),
     ],
   );
+
+
 
   runApp(const MyApp());
 }
@@ -100,15 +112,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -124,13 +127,91 @@ class _MyHomePageState extends State<MyHomePage> {
   late Timer _timer;
   bool _pause = true;
   String _stateText = "Au travail camarade !";
-
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  User? _currentUser;
+  int _sessionCount = 0;
+  int _totalTimeWorked = 0;
   @override
   void initState() {
     super.initState();
 
-    if (kIsWeb) {
+    // Demander la permission de notification
+    if (!kIsWeb) {
+      requestNotificationPermission();
+    } else {
       requestWebNotificationPermission();
+    }
+    _checkCurrentUser();
+  }
+  Future<void> _checkCurrentUser() async {
+    setState(() {
+      _currentUser = _auth.currentUser;
+    });
+  }
+
+  void _saveSession() async {
+    if (_currentUser != null) {
+      await FirebaseFirestore.instance.collection('sessions').add({
+        'userId': _currentUser!.uid,
+        'dateStart': FieldValue.serverTimestamp(),
+        'sessionCount': _sessionCount,
+        'totalTimeWorked': _totalTimeWorked,
+        'createdAt': FieldValue.serverTimestamp(),
+      });
+      print("Session sauvegardée pour l'utilisateur: ${_currentUser!.uid}");
+    } else {
+      print("Aucun utilisateur connecté.");
+    }
+  }
+
+  // Méthode pour se connecter avec Google
+  Future<void> _signInWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      // Créer un nouvel identifiant de connexion avec les informations Google
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Authentifier avec Firebase
+      UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      // Connexion réussie
+      print("Utilisateur connecté: ${userCredential.user?.email}");
+      setState(() {
+        _currentUser = userCredential.user;
+      });
+      // Vous pouvez naviguer vers une autre page ou mettre à jour l'interface utilisateur ici
+    } catch (e) {
+      print("Erreur lors de la connexion: $e");
+    }
+  }
+  Future<void> _signOut() async {
+    try {
+      await _auth.signOut();
+      await _googleSignIn.signOut();
+
+      // Mettre à jour l'état pour refléter que l'utilisateur est déconnecté
+      setState(() {
+        _currentUser = null;
+      });
+    } catch (e) {
+      print("Erreur lors de la déconnexion: $e");
+    }
+  }
+  void requestNotificationPermission() async {
+    // Vérifiez si les notifications sont autorisées
+    bool isAllowed = await AwesomeNotifications().isNotificationAllowed();
+    print('Notification permission allowed: $isAllowed'); // Debug log
+
+    if (!isAllowed) {
+      // Demande de permission
+      await AwesomeNotifications().requestPermissionToSendNotifications();
+      print('Notification permission requested'); // Debug log
     }
   }
 
@@ -150,12 +231,13 @@ class _MyHomePageState extends State<MyHomePage> {
     AwesomeNotifications().createNotification(
       content: NotificationContent(
         id: 1,
-        channelKey: 'pomodoro_channel',
+        channelKey: 'basic_channel',
         title: title,
         body: body,
         displayOnBackground: true,
         displayOnForeground: true,
         customSound: soundResource,
+        icon: 'resource://drawable/clock',
       ),
     );
   }
@@ -181,6 +263,8 @@ class _MyHomePageState extends State<MyHomePage> {
               _currentTime--; 
             } else if (_isWorking) {
               _isWorking = false;
+              _sessionCount++;
+              _totalTimeWorked=_totalTimeWorked+_workTime;
               _currentTime = _breakTime;
               _stateText = "Repos !";
               showNotification("Travail Terminé", "Il est temps de faire une pause !");
@@ -209,11 +293,20 @@ class _MyHomePageState extends State<MyHomePage> {
   void _resetTimer() {
     _timer.cancel();
     setState(() {
+      _sessionCount++;
+      if (_isWorking) {
+        _totalTimeWorked += (_workTime - _currentTime);
+      } else {
+        _totalTimeWorked += (_breakTime - _currentTime);
+      }
+      _saveSession();
       _currentTime = _workTime;
       _pause = true;
       _isRunning = false;
       _isWorking = true;
       _stateText = "Au travail camarade !";
+      _sessionCount = 0;
+      _totalTimeWorked = 0;
     });
   }
 
@@ -245,41 +338,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // Nouveau bouton pour la connexion Google
+            if (_currentUser != null)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Connecté en tant que ${_currentUser!.displayName}',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(height: 10), // Espace entre le texte et le bouton
+                  ElevatedButton(
+                    onPressed: _signOut, // Appeler la méthode de déconnexion
+                    child: const Text('Se déconnecter'),
+                  ),
+                ],
+              )
+            else
+              ElevatedButton(
+                onPressed: _signInWithGoogle, // Appeler la méthode de connexion
+                child: const Text('Se connecter avec Google'),
+              ),
+            SizedBox(height: 20), // Ajouter un espace pour un meilleur alignement
+
+            // Texte existant indiquant l'état actuel
             Text(
               _stateText,
               style: TextStyle(fontSize: 30),
@@ -289,48 +380,139 @@ class _MyHomePageState extends State<MyHomePage> {
               style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _startTimer,
-                  child: Text('Démarrer'),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _pauseTimer,
-                  child: Text('Pause'),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _resetTimer,
-                  child: Text('Reset'),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _45_15,
-                  child: Text('45/15'),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  onPressed: _25_5,
-                  child: Text('25/5'),
-                ),
-              ],
-            ),
+
+            // Boutons de contrôle du minuteur (Démarrer, Pause, Stop)
+            if (!_isRunning) ...[
+              // Afficher ces boutons lorsque le minuteur n'est pas en cours d'exécution
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _startTimer,
+                    child: Text('Démarrer'),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20),
+              // Boutons pour les paramètres de minuteur prédéfinis (45/15, 25/5)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _45_15,
+                    child: Text('45/15'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _25_5,
+                    child: Text('25/5'),
+                  ),
+                ],
+              ),
+            ] else ...[
+              // Afficher ces boutons lorsque le minuteur est en cours d'exécution
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: _pauseTimer,
+                    child: Text('Pause'),
+                  ),
+                  SizedBox(width: 10),
+                  ElevatedButton(
+                    onPressed: _resetTimer,
+                    child: Text('Stop'),
+                  ),
+                ],
+              ),
+            ],
+            SizedBox(height: 20), // Espace supplémentaire pour le bouton suivant
+
+            // Nouveau bouton pour ouvrir une page vide
+            if (_currentUser != null) // Check if the user is logged in
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => UserSessionsPage(userId: _currentUser!.uid)),
+                  );
+                },
+                child: const Text('Historique'),
+              )
           ],
         ),
       ),
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.*/
+    );
+  }
+}
+
+String formatDuration(int totalSeconds) {
+  final hours = totalSeconds ~/ 3600; // Integer division for hours
+  final minutes = (totalSeconds % 3600) ~/ 60; // Remaining minutes
+  final seconds = totalSeconds % 60; // Remaining seconds
+  return '${hours}h ${minutes}m ${seconds}s'; // Return formatted string
+}
+
+class UserSessionsPage extends StatelessWidget {
+  final String userId;
+
+  UserSessionsPage({required this.userId});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Historique des Timers'), // Changer le titre ici
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('sessions')
+            .where('userId', isEqualTo: userId)
+            .orderBy('createdAt', descending: true) // Maintien l'ordre pour afficher les sessions les plus récentes en premier
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return Center(child: Text("Aucun timer trouvé")); // Changer le texte ici
+          }
+
+          // Retrieve the sessions
+          final sessions = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: sessions.length,
+            itemBuilder: (context, index) {
+              final sessionData = sessions[index].data() as Map<String, dynamic>;
+              final dateStart = (sessionData['dateStart'] as Timestamp).toDate();
+              final sessionCount = sessionData['sessionCount'] ?? 0;
+              final totalTimeWorked = sessionData['totalTimeWorked'] ?? 0;
+
+              // Format the total time worked into hours, minutes, and seconds
+              final formattedTotalTime = formatDuration(totalTimeWorked);
+
+              // Format the date and time
+              final dateFormatter = DateFormat('dd MMMM yyyy à HH:mm:ss');
+
+              return Card(
+                child: ListTile(
+                  title: Text('Timer ${sessions.length - index}'), // Changer "Session" à "Timer" ici
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Débuté le: ${dateFormatter.format(dateStart)}'),
+                      Text('Nombre de sessions: $sessionCount'), // Changer "sessions" à "timers"
+                      Text('Temps total travaillé: $formattedTotalTime'), // Afficher le temps formaté
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
